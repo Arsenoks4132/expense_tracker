6ca97fe2ff91489b0c42db3604db31d0d8412fd6
Arseniy Danov <Danov2005@yandex.ru>
2025-05-28 23:58:49 +0300

    Add unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
+import { configDefaults } from "vitest/config"
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +9,13 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    include: ["**/*.{test,spec}.{ts,tsx}"],
+    exclude: [...configDefaults.exclude, "e2e"],
   },
   css: {
     modules: {
@@ -21,5 +29,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
-  },
+  }
 })
